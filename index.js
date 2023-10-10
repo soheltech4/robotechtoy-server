9d306f6d9ebc9496e4e80c826522b29c88e2d385
Sohel Rana <sohelranagtc@gmail.com>
2023-10-10 21:01:26 +0600

    get operation

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const cors = require('cors')
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
-
 //  middleware
 
 app.use(cors());
 app.use(express.json())
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.rbfkgiq.mongodb.net/?retryWrites=true&w=majority";
+console.log(process.env.DB_USER)
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.rbfkgiq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -24,16 +28,32 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const products = client.db("roboTech").collection("products");
+
+    app.get('/products', async(req, res)=>{
+      const cursor = products.find()
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+    app.get('/products/:id', async(req, res)=> {
+      const id = req.params.id 
+      const query = { _id : new ObjectId(id)}
+      const result = await products.findOne(query)
+      res.send(result)
+    })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', (req, res)=> {
