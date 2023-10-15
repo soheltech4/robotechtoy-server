5d1c9fbb7eb44cc6c8ef4163aa166e5b6682d95c
unknown <sohelranagtc@gmail.com>
2023-10-15 07:36:43 +0600

    added

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const cors = require('cors')
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
-//  middleware
 
+//  middleware
 const corsConfig = {
   origin: '',
   credentials: true,
